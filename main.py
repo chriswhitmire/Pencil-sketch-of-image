895ee6245707a091a6c5d7a4661676ca61687da0
chriswhitmire <chriswhitmire94@gmail.com>
2022-02-16 23:06:37 -0500

    Removed unnecessary import

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
-from turtle import pen
 import cv2
 #reading image
 img = cv2.imread("Assets/babies.jpg")
-# image = cv2.imread("Assets/christmasSam.jpg")
-# image = cv2.imread("Assets/nub.jpg")
+# img = cv2.imread("Assets/christmasSam.jpg")
+# img = cv2.imread("Assets/nub.jpg")
 
 # converting BGR image to grayscale
 grayImg = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
@@ -20,8 +19,8 @@ inverted_blurred = 255 - blurredImg
 
 # vars for scales for different images
 babiesScale = 200
-christmasScale = 200
-nubScale = 200
+christmasScale = 125
+nubScale = 150
 
 # divide the two images
 pencil_sketch = cv2.divide(grayImg, inverted_blurred, scale=babiesScale)
